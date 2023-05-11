9456482aab1628e7309b741b66b5140688d6cc7e
kbhole <kbhole@deloitte.com>
2023-05-11 18:17:00 +0530

    Read More button added on list screen

--- kmm/feature/news/src/commonMain/kotlin/com/example/news/View/NewsArticleItem.kt
+++ kmm/feature/news/src/commonMain/kotlin/com/example/news/View/NewsArticleItem.kt
@@ -3,10 +3,13 @@ package com.example.news.View
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.news.Model.NewsArticle
@@ -15,13 +18,16 @@ import com.example.news.Model.NewsArticle
 fun NewsArticleList(articles: List<NewsArticle>) {
     LazyColumn {
         items(articles.size) { index ->
-            NewsArticleItem(article = articles.get(index))
+
+            NewsArticleItem(article = articles.get(index)) { title, description ->
+                // Show the modal dialog
+            }
         }
     }
 }
 
 @Composable
-fun NewsArticleItem(article: NewsArticle) {
+fun NewsArticleItem(article: NewsArticle, onClick: (String, String)-> Unit) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -40,6 +46,12 @@ fun NewsArticleItem(article: NewsArticle) {
                 style = MaterialTheme.typography.body1,
                 modifier = Modifier.padding(top = 8.dp)
             )
+            Button(onClick = {
+                onClick(article.title, article.description)
+            }) {
+                Text("Read more")
+            }
         }
     }
 }
+
